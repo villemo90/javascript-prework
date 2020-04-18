2a94e1985f799b36eedb49b5133ca6a33eaa3c94
Sylwia Sniezko-Blocka <galka.sylwestra@gmail.com>
2020-04-18 23:42:59 +0200

    Add first game variables

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// test
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
