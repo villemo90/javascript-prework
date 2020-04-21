b7f52797bf7c6f2dc09d05310872153c536fbbc3
Sylwia Sniezko-Blocka <galka.sylwestra@gmail.com>
2020-04-21 23:38:19 +0200

    Add player movement for script.js

--- js/script.js
+++ js/script.js
@@ -15,4 +15,18 @@ if (randomNumber == '1') {
 } else if (randomNumber == '3') {
   computerMove = 'nożyce';
 }
-printMessage('Mój ruch: ' + computerMove);
+printMessage('komputer ruch: ' + computerMove);
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage('błędny wybór');
+}
+printMessage('mój ruch: ' + playerMove);
