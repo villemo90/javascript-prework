7e207dfb16ce968df2457280a6c8e586ae7dc360
Sylwia Sniezko-Blocka <galka.sylwestra@gmail.com>
2020-04-21 23:03:10 +0200

    Add else if for script.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ var computerMove, playerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
-printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
